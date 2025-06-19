f07bcca8eef610ba36c851f4c8f548460a21e807
Snezha0111 <130033841+Snezha0111@users.noreply.github.com>
2025-06-19 12:35:08 +0300

    new_req

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const jwt = require('jsonwebtoken')
 // })
 
 const SECRET_KEY = 'trud_work'
+const generate_token = function(id,email){
+    const token = {
+        id, email
+    }
+    return jwt.sign(token,SECRET_KEY)
+}
+
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -21,6 +28,49 @@ app.use(express.json())
 const PORT = process.env.PORT || 5001
 
 // Место для запросов
+// Регистрация
+app.post('/registration', async(req, res)=>{
+    try{
+        const{email, pass, fio, number} = req.body
+        const maybe_user = await db.oneOrNone('SELECT users.email FROM users WHERE email = $1', [email])
+        if(maybe_user){
+            return res.status(400).json({message: 'Пользователь уже существует'})
+        }
+        const hash_password = bcrypt.hashSync(pass, 7)
+        const new_user = await db.query(
+            `INSERT INTO users (email, pass, fio, number)`,
+            [email, pass, fio, number]
+        )
+        res.json(new_user)
+    }
+    catch(e){
+        console.log(e)
+        res.status(400).json({message: 'Ошибка регистрации'})
+    }
+})
+
+// Авторизация
+app.post('/authorization', async(req,res)=>{
+    try{
+        const {email, pass} = req.body
+        const maybe_user = await db.oneOrNone('SELECT users.email FROM users WHERE email = $1', [email])
+        if(!maybe_user){
+            return res.status(400).json({message: 'Пользователь не найден'})
+        }
+        const valid_pass = bcrypt.compareSync(pass, maybe_user.pass)
+        if(!valid_pass){
+            return res.status(400).json({message: 'Введени неверный пароль'})
+        }
+        const token = generate_token(maybe_user.id_user, maybe_user.email) 
+        res.json(token)
+    }
+    catch(e){
+        console.log(e)
+        res.status(400).json({message: 'Ошибка авторизации'})
+    }
+})
+
+// Вывод заявок
 
 
 
