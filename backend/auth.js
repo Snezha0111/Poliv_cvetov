7d5cead8a075fde1acb2a1f1add0405c8351b95e
Snezha0111 <130033841+Snezha0111@users.noreply.github.com>
2025-06-19 23:04:40 +0300

    new_back

--- backend/auth.js
+++ backend/auth.js
@@ -9,3 +9,30 @@ switch_reg.addEventListener('click', function(){
     }
 })
 
+// Авторизация
+const log_in = document.getElementById("but_auth")
+log_in.addEventListener('click', ()=>{
+    const inp_log= document.getElementById('log_auth').value
+    const inp_pass = document.getElementById('pass_auth').value
+    if(inp_log == 'sklad' && inp_pass == '123qwe'){
+        window.location.href = "admin.html"
+    }
+    fetch('http://localhost:5001/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: inp_log, password: inp_pass})
+    })
+    .then(response =>{
+        if(response.ok){
+            console.log('Пользователь авторизован');
+            // alert('Пользователь авторизован')
+        }
+        else{
+            alert('Пользователя с таким email или паролем не существует')
+        }
+    }).catch(error => {
+        console.error('Ошибка:', error)
+    })
+})
