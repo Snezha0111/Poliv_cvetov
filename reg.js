0012eb8dbcdb683743aa2b08a521dfad9eb9bb1a
Snezha0111 <130033841+Snezha0111@users.noreply.github.com>
2025-06-19 23:01:26 +0300

    Delete reg.js

--- reg.js
+++
@@ -1,9 +0,0 @@
-const switch_auth = document.getElementById("but_per_auth")
-switch_auth.addEventListener('click', function(){
-    if (switch_auth){
-        window.location.href = "auth.html"
-    }
-    else{
-        alert('что-то пошло не так')
-    }
-})
