ddeec86a31113ea904386cfefbfd3f1955893fe1
Snezha0111 <130033841+Snezha0111@users.noreply.github.com>
2025-06-20 09:10:23 +0300

    Update reg.js
    Fio

--- backend/reg.js
+++ backend/reg.js
@@ -7,3 +7,27 @@ switch_auth.addEventListener('click', function(){
         alert('что-то пошло не так')
     }
 })
+
+//document.getElementById('registrationForm').addEventListener('submit', function(event) {
+   // event.preventDefault(); 
+
+   // const formData = new FormData(this); 
+  //  let fullNameArray = formData.get("fullName").split(/\s+/); 
+
+  //  if(fullNameArray.length !== 3){
+   //     alert("Ошибка! ФИО должно содержать ровно три слова.");
+    //    return false;
+ //   }
+
+ //   const dataToSend = {
+    //    login: formData.get("login"),
+    //    password: formData.get("password"),
+      //  firstName: fullNameArray[1],   
+       // middleName: fullNameArray[2],  
+      //  lastName: fullNameArray[0],    
+     //   phoneNumber: formData.get("phoneNumber")
+ //   };
+
+    console.log(dataToSend); 
+
+});
